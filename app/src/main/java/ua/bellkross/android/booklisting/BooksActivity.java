12922110af3d42a609b9409bbe092c93b0dd074b
Bellkross <vasilenkowm@yandex.ua>
2018-01-12 11:24:28 +0200

    Added searchView

--- app/src/main/java/ua/bellkross/android/booklisting/BooksActivity.java
+++ app/src/main/java/ua/bellkross/android/booklisting/BooksActivity.java
@@ -2,6 +2,10 @@ package ua.bellkross.android.booklisting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+
+import java.util.zip.Inflater;
 
 public class BooksActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class BooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_books, menu);
+        return true;
     }
 }
